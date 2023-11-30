bc113babe55b91bd9c0109e579b21b15083c4d9e
LoganBojarski <bojarski@pdx.edu>
2023-11-29 19:56:32 -0800

    helped

--- bobacat/script.js
+++ bobacat/script.js
@@ -12,8 +12,3 @@
     circleCursor.style.left = `${e.pageX}px`;
     circleCursor.style.top = `${e.pageY}px`;
   });
-//button to image
-function scrollToImage() {
-    const imageContainer = document.getElementById('image-container');
-    imageContainer.scrollIntoView({ behavior: 'smooth' });
-  }
